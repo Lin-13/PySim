24e70bf0e066255c3ea3dffea3dc1a229a4e45ba
lwx <3065746398@qq.com>
2023-11-14 20:38:00 +0800

    package rename  and build as pysim

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+    name="pysim",
+    version="0.5",
+    description="A python lib for dynamic system simulator, "+
+    "including *Transfer Function*,*State Space Function* and "+
+    "simple *Dynamic system Gragh* modules "+
+    "and other essential utils",
+    packages=find_packages()
+)
